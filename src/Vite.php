54b8b6c7722483674d05f74897e7ca4029926284
Phil Quinn <phil@quinn.com>
2026-03-24 12:11:45 -0700

    fix paths to allow requirements cache busting urls

--- src/Vite.php
+++ src/Vite.php
@@ -2,6 +2,7 @@
 
 namespace Somar\Vite;
 
+use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
 use SilverStripe\Core\Environment;
 use SilverStripe\Core\Injector\Injectable;
@@ -16,11 +17,6 @@ use SilverStripe\View\Requirements;
 class Vite implements RequirementsInterface
 {
     use Injectable;
-
-    private static $dependencies = [
-        'manifestProvider' => '%$' . ManifestProvider::class,
-        'resourceURLGenerator' => '%$' . ResourceURLGenerator::class,
-    ];
 
     public ManifestProvider $manifestProvider;
 
@@ -57,88 +53,10 @@ class Vite implements RequirementsInterface
      */
     private array $disabledNonceFiles = [];
 
-    /**
-     * Manually initialise the Vite dev server. This is useful for when you want
-     * to insert the react refresh runtime script.
-     */
-    public static function configDevServer(bool $isReact = false): void
-    {
-        self::singleton()->initDevServer();
-
-        if ($isReact) {
-            self::singleton()->insertReactRefresh();
-        }
-    }
-
-    /**
-     * Register the given JavaScript file as required.
-     *
-     * This will also recursivly load any imports/css from the chunk as per vite
-     * manifest specs
-     *
-     * @param string $file The javascript file to load, relative to site root
-     * @param array $options List of options. Available options include:
-     * - 'preload' : Preload the resource (defaults to true)
-     * - 'provides' : List of scripts files included in this file
-     * - 'async' : Boolean value to set async attribute to script tag
-     * - 'defer' : Boolean value to set defer attribute to script tag
-     * - 'type' : Override script type= value.
-     * - 'integrity' : SubResource Integrity hash
-     * - 'crossorigin' : Cross-origin policy for the resource
-     */
-    public static function javascript(string $file, array $options = []): void
-    {
-        self::singleton()->requireJavascript($file, $options);
-    }
-
-    /**
-     * Register the given stylesheet into the list of requirements.
-     *
-     * @param string $file The CSS file to load, relative to site root
-     * @param string $media Comma-separated list of media types to use in the link tag
-     *                      (e.g. 'screen,projector')
-     * @param array $options List of options. Available options include:
-     * - 'preload' : Preload the resource (defaults to true)
-     * - 'integrity' : SubResource Integrity hash
-     * - 'crossorigin' : Cross-origin policy for the resource
-     */
-    public static function css(string $file, ?string $media = null, array $options = []): void
-    {
-        self::singleton()->requireCss($file, $media, $options);
-    }
-
-    /**
-     * Preload the given file into the head of the document.
-     *
-     * @param string $file The file to preload, relative to site root
-     * @param string $as The type of resource to preload
-     *                   (e.g. 'script', 'style', 'font', 'image', 'document')
-     * @param string $type The MIME type of the resource
-     */
-    public static function preload(string $file, ?string $as = null, ?string $type = null): void
-    {
-        self::singleton()->requirePreload($file, $as, $type);
-    }
-
-    /**
-     * Resolve resource path from the manifest
-     */
-    public static function resourcePath(string $resource): ?string
-    {
-        return static::singleton()
-            ->getManifestProvider()
-            ->resolvePath($resource);
-    }
-
-    /**
-     * Resolve resource URL from the manifest
-     */
-    public static function resourceURL(string $resource): ?string
-    {
-        return static::singleton()
-            ->getManifestProvider()
-            ->resolveURL($resource);
-    }
+    private static $dependencies = [
+        'manifestProvider'     => '%$' . ManifestProvider::class,
+        'resourceURLGenerator' => '%$' . ResourceURLGenerator::class,
+    ];
 
     public function __construct()
     {
@@ -231,138 +149,6 @@ class Vite implements RequirementsInterface
     }
 
     /**
-     * Add a JS file to the list of files to be loaded
-     *
-     * This will also recursivly load any imports/css from the chunk as per vite
-     * manifest specs
-     */
-    protected function addJsFromManifest(string $file, array $options = [], &$preloads = []): void
-    {
-        $resource = $this->manifestProvider->resolveResource($file);
-        if (!$resource) {
-            return;
-        }
-
-        $path = $this->manifestProvider->resolvePath($resource);
-        if (!$path) {
-            return;
-        }
-
-        $this->addDisabledNonceFile($file, $path);
-        $preloads[$file] = [
-            'path' => $path,
-            'as' => 'script',
-        ];
-
-        foreach ($resource['imports'] ?? [] as $import) {
-            $this->addJsFromManifest($import, $preloads);
-        }
-
-        foreach ($resource['css'] ?? [] as $css) {
-            // The css is not necessarily an entry point so we need to dummy
-            // resolve it to get the full path
-            $cssPath = $this->manifestProvider->resolvePath([
-                'file' => $css,
-            ]);
-            $this->addDisabledNonceFile($css, $cssPath);
-            $preloads[$css] = [
-                'path' => $cssPath,
-                'as' => 'style',
-            ];
-
-            Requirements::css($cssPath);
-        }
-
-        Requirements::javascript($path, [
-            'type' => 'module',
-            ...$options,
-        ]);
-    }
-
-    /**
-     * Add a CSS file to the list of files to be loaded
-     */
-    protected function addCssFromManifest(
-        string $file,
-        ?string $media = null,
-        array $options = [],
-        array &$preloads = []
-    ): void {
-        $path = $this->manifestProvider->resolvePath($file);
-
-        if (!$path) {
-            return;
-        }
-
-        $this->addDisabledNonceFile($file, $path);
-        $preloads[$file] = [
-            'path' => $path,
-            'as' => 'style',
-        ];
-
-        Requirements::css($path, $media, $options);
-    }
-
-    /**
-     * Preload a file from the manifest
-     */
-    protected function preloadFileFromManifest(
-        string $file,
-        string $as,
-        ?string $type = null,
-        array &$preloads = []
-    ): void {
-        $path = $this->manifestProvider->resolvePath($file);
-
-        if (!$path) {
-            return;
-        }
-
-        $this->disabledNonceFiles[$file] = $path;
-        $preloads[$file] = [
-            'path' => $path,
-            'as' => $as,
-            'type' => $type,
-        ];
-    }
-
-    /**
-     * Determine whether the given path is a JS file.
-     */
-    protected function isJsPath(string $path): bool
-    {
-        return preg_match('/\.(js|cjs|mjs)$/', $path) === 1;
-    }
-
-    /**
-     * Insert preload tags into the head for the given files
-     */
-    protected function insertPreloadTags(array $files): void
-    {
-        $tags = $this->createPreloadTags($files);
-        // Add preload tags to head
-        foreach ($tags as $tag) {
-            Requirements::insertHeadTags($tag);
-        }
-    }
-
-    /**
-     * Create preload tags for the given files
-     */
-    protected function createPreloadTags(array $files): array
-    {
-        $tags = [];
-        // Add preload tags to head
-        foreach (array_values($files) as $file) {
-            $tag = $this->createPreloadTag($file);
-            if ($tag) {
-                $tags[] = $tag;
-            }
-        }
-        return $tags;
-    }
-
-    /**
      * Create preload tags for the given files
      */
     public function createPreloadTag(array $file): ?string
@@ -382,8 +168,8 @@ class Vite implements RequirementsInterface
         unset($file['path']);
 
         $tag = HTML::createTag('link', [
-            'rel' => $rel,
-            'href' => $fullPath,
+            'rel'         => $rel,
+            'href'        => $fullPath,
             'crossorigin' => true,
             ...$file,
         ]);
@@ -415,34 +201,6 @@ class Vite implements RequirementsInterface
         ));
 
         $this->reactRefreshRuntimeInit = true;
-    }
-
-    /**
-     * Initialize the Vite dev server and check if it's running
-     */
-    protected function initDevServer(): void
-    {
-        // Allow lazy initialization
-        if ($this->devServerInit) {
-            return;
-        }
-
-        // Set the flag to prevent re-initialization
-        $this->devServerInit = true;
-
-        // We don't want to run this in live mode
-        if (Director::isLive()) {
-            return;
-        }
-
-        if (!$this->isDevServerRunning()) {
-            return;
-        }
-
-        // Add the vite client to the head
-        Requirements::javascript($this->devServerResourceUrl('@vite/client'), [
-            'type' => 'module'
-        ]);
     }
 
     /**
@@ -547,5 +305,265 @@ class Vite implements RequirementsInterface
     public function getManifestProvider(): ManifestProvider
     {
         return $this->manifestProvider;
+    }
+
+    /**
+     * Add a JS file to the list of files to be loaded
+     *
+     * This will also recursivly load any imports/css from the chunk as per vite
+     * manifest specs
+     */
+    protected function addJsFromManifest(string $file, array $options = [], &$preloads = []): void
+    {
+        $resource = $this->manifestProvider->resolveResource($file);
+        if (!$resource) {
+            return;
+        }
+
+        $path = $this->manifestProvider->resolvePath($resource);
+        if (!$path) {
+            return;
+        }
+
+        $this->addDisabledNonceFile($file, $path);
+        $preloads[$file] = [
+            'path' => $path,
+            'as'   => 'script',
+        ];
+
+        foreach ($resource['imports'] ?? [] as $import) {
+            $this->addJsFromManifest($import, $preloads);
+        }
+
+        $resourcesPath = Controller::join_links(
+            Director::publicDir(),
+            RESOURCES_DIR
+        );
+
+        foreach ($resource['css'] ?? [] as $css) {
+            // The css is not necessarily an entry point so we need to dummy
+            // resolve it to get the full path
+            $cssPath = $this->manifestProvider->resolvePath([
+                'file' => $css,
+            ]);
+            $this->addDisabledNonceFile($css, $cssPath);
+            $preloads[$css] = [
+                'path' => $cssPath,
+                'as'   => 'style',
+            ];
+
+            $cssPath = preg_replace("{^{$resourcesPath}/}", '', $cssPath);
+
+            Requirements::css($cssPath);
+        }
+
+        // remove public folder prefix
+        $path = preg_replace("{^{$resourcesPath}/}", '', $path);
+
+        Requirements::javascript($path, [
+            'type' => 'module',
+            ...$options,
+        ]);
+    }
+
+    /**
+     * Add a CSS file to the list of files to be loaded
+     */
+    protected function addCssFromManifest(
+        string $file,
+        ?string $media = null,
+        array $options = [],
+        array &$preloads = []
+    ): void {
+        $path = $this->manifestProvider->resolvePath($file);
+
+        if (!$path) {
+            return;
+        }
+
+        $this->addDisabledNonceFile($file, $path);
+        $preloads[$file] = [
+            'path' => $path,
+            'as'   => 'style',
+        ];
+
+        // remove public folder prefix
+        $resourcesPath = Controller::join_links(
+            Director::publicDir(),
+            RESOURCES_DIR
+        );
+        $path = preg_replace("{^{$resourcesPath}/}", '', $path);
+
+        Requirements::css($path, $media, $options);
+    }
+
+    /**
+     * Preload a file from the manifest
+     */
+    protected function preloadFileFromManifest(
+        string $file,
+        string $as,
+        ?string $type = null,
+        array &$preloads = []
+    ): void {
+        $path = $this->manifestProvider->resolvePath($file);
+
+        if (!$path) {
+            return;
+        }
+
+        $this->disabledNonceFiles[$file] = $path;
+        $preloads[$file] = [
+            'path' => $path,
+            'as'   => $as,
+            'type' => $type,
+        ];
+    }
+
+    /**
+     * Determine whether the given path is a JS file.
+     */
+    protected function isJsPath(string $path): bool
+    {
+        return preg_match('/\.(js|cjs|mjs)$/', $path) === 1;
+    }
+
+    /**
+     * Insert preload tags into the head for the given files
+     */
+    protected function insertPreloadTags(array $files): void
+    {
+        $tags = $this->createPreloadTags($files);
+        // Add preload tags to head
+        foreach ($tags as $tag) {
+            Requirements::insertHeadTags($tag);
+        }
+    }
+
+    /**
+     * Create preload tags for the given files
+     */
+    protected function createPreloadTags(array $files): array
+    {
+        $tags = [];
+        // Add preload tags to head
+        foreach (array_values($files) as $file) {
+            $tag = $this->createPreloadTag($file);
+            if ($tag) {
+                $tags[] = $tag;
+            }
+        }
+        return $tags;
+    }
+
+    /**
+     * Initialize the Vite dev server and check if it's running
+     */
+    protected function initDevServer(): void
+    {
+        // Allow lazy initialization
+        if ($this->devServerInit) {
+            return;
+        }
+
+        // Set the flag to prevent re-initialization
+        $this->devServerInit = true;
+
+        // We don't want to run this in live mode
+        if (Director::isLive()) {
+            return;
+        }
+
+        if (!$this->isDevServerRunning()) {
+            return;
+        }
+
+        // Add the vite client to the head
+        Requirements::javascript($this->devServerResourceUrl('@vite/client'), [
+            'type' => 'module'
+        ]);
+    }
+
+    /**
+     * Manually initialise the Vite dev server. This is useful for when you want
+     * to insert the react refresh runtime script.
+     */
+    public static function configDevServer(bool $isReact = false): void
+    {
+        self::singleton()->initDevServer();
+
+        if ($isReact) {
+            self::singleton()->insertReactRefresh();
+        }
+    }
+
+    /**
+     * Register the given JavaScript file as required.
+     *
+     * This will also recursivly load any imports/css from the chunk as per vite
+     * manifest specs
+     *
+     * @param string $file The javascript file to load, relative to site root
+     * @param array $options List of options. Available options include:
+     * - 'preload' : Preload the resource (defaults to true)
+     * - 'provides' : List of scripts files included in this file
+     * - 'async' : Boolean value to set async attribute to script tag
+     * - 'defer' : Boolean value to set defer attribute to script tag
+     * - 'type' : Override script type= value.
+     * - 'integrity' : SubResource Integrity hash
+     * - 'crossorigin' : Cross-origin policy for the resource
+     */
+    public static function javascript(string $file, array $options = []): void
+    {
+        self::singleton()->requireJavascript($file, $options);
+    }
+
+    /**
+     * Register the given stylesheet into the list of requirements.
+     *
+     * @param string $file The CSS file to load, relative to site root
+     * @param string $media Comma-separated list of media types to use in the link tag
+     *                      (e.g. 'screen,projector')
+     * @param array $options List of options. Available options include:
+     * - 'preload' : Preload the resource (defaults to true)
+     * - 'integrity' : SubResource Integrity hash
+     * - 'crossorigin' : Cross-origin policy for the resource
+     */
+    public static function css(string $file, ?string $media = null, array $options = []): void
+    {
+        self::singleton()->requireCss($file, $media, $options);
+    }
+
+    /**
+     * Preload the given file into the head of the document.
+     *
+     * @param string $file The file to preload, relative to site root
+     * @param string $as The type of resource to preload
+     *                   (e.g. 'script', 'style', 'font', 'image', 'document')
+     * @param string $type The MIME type of the resource
+     */
+    public static function preload(string $file, ?string $as = null, ?string $type = null): void
+    {
+        self::singleton()->requirePreload($file, $as, $type);
+    }
+
+    /**
+     * Resolve resource path from the manifest
+     */
+    public static function resourcePath(string $resource): ?string
+    {
+        return static::singleton()
+            ->getManifestProvider()
+            ->resolvePath($resource);
+    }
+
+    /**
+     * Resolve resource URL from the manifest
+     */
+    public static function resourceURL(string $resource): ?string
+    {
+        return static::singleton()
+            ->getManifestProvider()
+            ->resolveURL($resource);
     }
 }
